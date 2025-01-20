ea211348413628d45212a84a574621c497b79556
austinwilson1296 <austinlax20@gmail.com>
2025-01-20 13:25:57 -0500

    another

--- scripts/db_migration.sh
+++ scripts/db_migration.sh
@@ -1,16 +1,4 @@
 #!/bin/bash
-set -e
-
-
-
-# Debugging: Check if the variable is set
-echo "PROD_DB_URL is set to: $DB_URL"
-
-# Check if PROD_DB_URL is empty
-if [ -z "$DB_URL" ]; then
-    echo "Error: PROD_DB_URL is not set."
-    exit 1
-fi
 
 # Proceed with migration
 cd sql/schema
